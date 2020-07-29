29ccd07b99fc664ca99d83c60cba46f6e4e8612f
Anurag <anuragsekhri@gmail.com>
2020-07-29 22:27:09 +0530

    4interns

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,6 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyDEGd2cV2oIE3PiK6B-bgqbel8XlSPPylM",
-    authDomain: "medico-cms.firebaseapp.com",
-    databaseURL: "https://medico-cms.firebaseio.com",
-    projectId: "medico-cms",
-    storageBucket: "medico-cms.appspot.com",
-    messagingSenderId: "453184802282",
-    appId: "1:453184802282:web:b561d52b0c5c891fe03693",
-    measurementId: "G-FNMPMHEG8B"
+    // paste your db json here
   }
 };
